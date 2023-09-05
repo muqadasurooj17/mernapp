74ed4ff0530a46a0e161e619b1ff30530e556230
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-05 11:42:36 +0500

    created navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        helllo world
-      </h1>
+    <>
+    <Navbar/>
+    </>
     </div>
   );
 }
