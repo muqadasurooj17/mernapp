0551d1777e02dcd91d5bd816cc29540178f3d336
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-05 13:07:56 +0500

    added footer and careouel

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
+import Carouesal from "../components/Carouesal";
 
 export default function Home() {
   return (
     <div>
       <div>
         <Navbar />
+        <Carouesal/>
    <Card/>
         <Footer />
       </div>
