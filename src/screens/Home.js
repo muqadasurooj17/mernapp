3c357bd80813acb149b78b48fc1f4c42e4084d24
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-05 12:45:40 +0500

    added drop down

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -9,22 +9,24 @@ export default function Home() {
 
     <Navbar/>
     <div>
-    <div className="card" style="width: 18rem;">
+    <div className="card mt-3" style={{"width": "18rem", "maxhieght": "360px"}}>
   <img src="..." className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p className="card-text">Some quick example text to</p>
+    <div className='container w-100'>
+        <select className='m-2 h-100 w-100 bg-success'>
+        {Array.from(Array(10), (e,i)=>{
+            return(
+                <option key={i+1} value={i+1} >
+                    {i+1}
+                     </option>        )}  )}
+        </select> 
+        </div>
   </div>
 </div>
-    </div>
-
-    <Footer/>
-
-</div>
-
-
-
-
+   
+    </div><Footer/></div>
     </div>
   )
 }
