d95af52364e7b51cd728bb2dcf6ba80932345be0
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-05 11:55:43 +0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
-import Navbar from './components/Navbar';
+import Home from './screens/Home';
 
 function App() {
   return (
     <div className="App">
     <>
-    <Navbar/>
+   <Home/>
     </>
     </div>
   );
---
+++ src/screens/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <div>
+<div>
+
+    <Navbar/>
+    <div>
+        BOdy
+    </div>
+
+    <Footer/>
+    
+</div>
+
+
+
+
+    </div>
+  )
+}
