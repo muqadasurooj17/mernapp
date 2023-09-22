5577c7dd381a099de3b3909b2b8b6b9149e81d24
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-22 19:04:04 +0500

    done for now

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import Signup from './screens/Signup';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
   <Routes>
     <Route exact path='/' element={<Home/>}/>
     <Route exact path='/login' element={<Login/>}/>
+    <Route exact path='/login' element={<Signup/>}/>
   </Routes>
 </div>
   
