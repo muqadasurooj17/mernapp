fe3d9cc113e087234c4a03bb4c006a97d882426b
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-22 12:52:25 +0500

    connection created

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,30 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const mongoDB=require("./db");
-mongoDB();
+// 
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// Connect to MongoDB using Mongoose
+mongoose.connect('mongodb://muqadasurooj:gOtkpUexqWb6xdoY@ac-eovanbl-shard-00-00.f0qegyz.mongodb.net:27017,ac-eovanbl-shard-00-01.f0qegyz.mongodb.net:27017,ac-eovanbl-shard-00-02.f0qegyz.mongodb.net:27017/?ssl=true&replicaSet=atlas-c22rte-shard-0&authSource=admin&retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    startServer();
+  })
+  .catch(error => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// Define routes and middleware here
+
+function startServer() {
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
+}
