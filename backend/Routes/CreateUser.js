4ec9c5db09ab3775008cb7c2b7ffcf76a4bdff33
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-23 11:08:17 +0500

    doing

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -3,27 +3,26 @@ const router=express.Router()
 const User= require('../models/User')
 const {body,validationResult}=require('express-validator');
 
-
+const bcrypt=require("bcryptjs");
 router.post("/createuser",
 
 [body('email').isEmail(),
 body('name').isLength({min:5}),
 body('password','Please put 5 character').isLength({min:5})]
 , async(req,res)=>{
-    console.log(req.body.name,
-        req.body.password,
-        req.body.email,
-        req.body.location,)
+    
 
     const errors=validationResult(req);
 if(!errors.isEmpty()){
 return res.status(400).json({errors:errors.array()})
 }
 
+const salt=await bcrypt.genSalt(10);
+let secPassword= await bcrypt.hash(req.body.password,salt)
     try {
         await User.create({
             name:req.body.name,
-            password:req.body.password,
+            password:secPassword,
             email:req.body.email,
             location:req.body.location,
 
