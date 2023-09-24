e5d0c72e80f959b625239cd5b339c096bbec7202
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-24 10:19:04 +0500

    added footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+// import React from 'react'
+// import { Link } from 'react-router-dom'
 
+// export default function Footer() {
+//   return (
+//     <div><footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+//     <div class="col-md-4 d-flex align-items-center">
+//       <Link href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"> </Link>
+//       <span class="text-muted fs-4">© 2023 GoFood, Inc</span>
+//     </div>
+
+//   </footer></div>
+//   )
+// }
+import React from 'react'
 export default function Footer() {
   return (
-    <div><footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <div class="col-md-4 d-flex align-items-center">
-      <Link href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"> </Link>
-      <span class="text-muted fs-4">© 2023 GoFood, Inc</span>
-    </div>
-
-  </footer></div>
+    <div><div className="container">
+    <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <div className="col-md-4 d-flex align-items-center">
+        <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+        </a>
+        <span className="text-muted">© 2022 <i>GoFood</i>, Inc</span>
+      </div>
+  
+      <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+        <li className="ms-3"><a className="text-muted" href="/"><svg className="bi" width="24" height="24"><use ></use></svg></a></li>
+        <li className="ms-3"><a className="text-muted" href="/"><svg className="bi" width="24" height="24"><use ></use></svg></a></li>
+        <li className="ms-3"><a className="text-muted" href="/"><svg className="bi" width="24" height="24"><use></use></svg></a></li>
+      </ul>
+    </footer>
+  </div>
+  </div>
   )
 }
